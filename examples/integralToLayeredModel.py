229ca3331567200ae5a5c08c621a08b052f64b6c
Carsten Uphoff <uphoff@in.tum.de>
2017-07-31 17:55:05 +0200

    Cleanup

--- examples/integralToLayeredModel.py
+++ examples/integralToLayeredModel.py
@@ -22,31 +22,3 @@ print('nodes:')
 for i in range(len(layers)-1):
   print('  {}: [{}]'.format(layers[i], integral))
   integral = integral + (layers[i+1]-layers[i]) * rho[i] * 9.8
-#~ print('  map')
-#~ model: !LayeredModel
-  #~ map: !AffineMap
-    #~ matrix:
-      #~ z: [0.0, 0.0, 1.0]
-    #~ translation:
-      #~ z: 0
-  #~ interpolation: linear
-  #~ parameters: [rho]
-  #~ nodes:
-    #~ 0:     [2600, 1.04e10, 2.08e10]
-    #~ -2000: [2700, 3.24e10, 3.24e10]
-    #~ -6000: [3000, 4.80e10, 1.20e10]
-    #~ -12000: [
-    #~ -23000:
-    #~ -.inf:
-         #~ zLayers (1:6) = (/ 0d0,-2000d0, -6000d0, -12000d0, -23000d0,-600d6 /)
-          #~ rhoLayers (1:6) = (/ 1000d0, 2720d0, 2860d0, 3050d0, 3300d0, 3375d0 /)
-          #~ sigzz = 0d0
-#~ 
-          #~ DO k=2,nLayers
-             #~ IF (zGP.GT.zLayers(k)) THEN
-                #~ sigzz = sigzz + rhoLayers(k-1)*(zGP-zLayers(k-1))*g
-                #~ EXIT
-             #~ ELSE
-                #~ sigzz = sigzz + rhoLayers(k-1)*(zLayers(k)-zLayers(k-1))*g
-             #~ ENDIF
-#~ ENDDO
