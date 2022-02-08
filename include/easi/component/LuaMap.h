666052973d9e810a5151e7a887c1932be6047b39
Lukas Krenz <lukas@krenz.land>
2022-02-08 17:13:50 +0100

    Optimize Lua by compiling just once

--- include/easi/component/LuaMap.h
+++ include/easi/component/LuaMap.h
@@ -13,28 +13,28 @@
 #include "easi/util/FunctionWrapper.h"
 #include "easi/util/Matrix.h"
 
+// Forward declaration to avoid including all Lua headers
 struct lua_State;
 
 namespace easi {
+double getField(lua_State* L, const std::string& key);
+
 class LuaMap : public Map {
 public:
-    typedef std::map<std::string, std::string> OutMap;
-
-    virtual ~LuaMap() {}
-
     void setMap(const std::set<std::string>& in,
                 const std::set<std::string>& returns,
                 const std::string& function);
 
+    ~LuaMap() override;
 protected:
-    virtual Matrix<double> map(Matrix<double>& x);
+    virtual Matrix<double> map(Matrix<double>& x) override;
 
 private:
     double executeLuaFunction(Matrix<double> x, unsigned coordIdx, unsigned funcIdx);
     std::string function;
     std::vector<std::string> idxToNameMap;
+    lua_State* luaState;
 
-    double getField(lua_State* L, const std::string& key);
 };
 
 } // namespace easi
--- src/component/LuaMap.cpp
+++ src/component/LuaMap.cpp
@@ -13,42 +13,58 @@ extern "C" {
 
 namespace easi {
 
+double getField(lua_State* L, const std::string& key) {
+    lua_pushstring(L, key.data());
+    lua_gettable(L, -2);
+    if (!lua_isnumber(L, -1)) {
+        std::cerr
+            << "Tried key "
+            << key
+            << " but it did not return a number."
+            << std::endl;
+    }
+    const auto result = lua_tonumber(L, -1);
+    lua_pop(L, 1);
+    return result;
+}
+
 double LuaMap::executeLuaFunction(Matrix<double> xin,
                                   unsigned coordIdx,
                                   unsigned funcIdx) {
-    auto L = luaL_newstate();
-    luaL_openlibs(L);
-    const auto status = luaL_dostring(L, function.data());
-    if (status) {
-        std::cerr
-            << "Couldn't load script: "
-            << lua_tostring(L, -1);
-        std::abort();
+    if (!luaState) {
+        std::cout << "Init lua state" << std::endl;
+        luaState = luaL_newstate();
+        luaL_openlibs(luaState);
+        const auto status = luaL_dostring(luaState, function.data());
+        if (status) {
+            std::cerr
+                << "Couldn't load script: "
+                << lua_tostring(luaState, -1);
+            std::abort();
+        }
     }
-
-    double z;
 
     /* push functions and arguments */
-    lua_newtable(L);
+    lua_newtable(luaState);
     for (int i = 0; i < 3; ++i) {
-        lua_pushnumber(L, i+1);
-        lua_pushnumber(L, xin(coordIdx, i));
-        lua_rawset(L, -3);
+        lua_pushnumber(luaState, i+1);
+        lua_pushnumber(luaState, xin(coordIdx, i));
+        lua_rawset(luaState, -3);
     }
-    lua_pushstring(L, "test");
-    lua_pushnumber(L, 42);
-    lua_rawset(L, -3);
-    lua_setglobal(L, "x");
-    lua_getglobal(L, "f");  /* function to be called */
-    lua_getglobal(L, "x");
+    lua_pushstring(luaState, "test");
+    lua_pushnumber(luaState, 42);
+    lua_rawset(luaState, -3);
+    lua_setglobal(luaState, "x");
+    lua_getglobal(luaState, "f");  /* function to be called */
+    lua_getglobal(luaState, "x");
 
-    if (lua_pcall(L, 1, 1, 0) != 0) {
+    if (lua_pcall(luaState, 1, 1, 0) != 0) {
         std::cerr
         << "Error running function f "
-        << lua_tostring(L, -1)
+        << lua_tostring(luaState, -1)
         << std::endl;
     }
-    return getField(L, idxToNameMap[funcIdx]);
+    return getField(luaState, idxToNameMap[funcIdx]);
 }
 
 Matrix<double> LuaMap::map(Matrix<double>& x) {
@@ -75,20 +91,13 @@ void LuaMap::setMap(const std::set<std::string>& in,
     }
 }
 
-double LuaMap::getField(lua_State* L, const std::string& key) {
-    lua_pushstring(L, key.data());
-    lua_gettable(L, -2);
-    if (!lua_isnumber(L, -1)) {
-        std::cerr
-            << "Tried key "
-            << key
-            << " but it did not return a number."
-            << std::endl;
-    }
-    const auto result = lua_tonumber(L, -1);
-    lua_pop(L, 1);
-    return result;
+LuaMap::~LuaMap() {
+   if (luaState) {
+       lua_close(luaState);
+   }
 }
+
+
 
 } // namespace easi
 #endif // EASI_USE_LUA
