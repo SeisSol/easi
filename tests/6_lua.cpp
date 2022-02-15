486d08933b8c0852a6fe242ef53fdaa76069c1ee
Lukas Krenz <lukas@krenz.land>
2022-02-15 17:03:25 +0100

    Use correct formula

--- tests/6_lua.cpp
+++ tests/6_lua.cpp
@@ -1,29 +1,30 @@
 #include "easitest.h"
 #include <cmath>
-#include <iostream>
 
 double effectiveConfiningStress(double z) {
     const double elevation = 100;
-    return 2670.0*0.21*9.8*std::min(-100.0,elevation-z);
+    return 2670.0 * 0.21 * 9.8 * std::min(-100.0, elevation + z);
 }
 
 int main(int argc, char** argv) {
     assert(argc == 2);
 
     easi::Query query = createQuery<3>({
-        {0, {0.0, 0.0, 200.0}},
-        {0, {0.0, 0.0, 210.0}},
-        {0, {0.0, 0.0, 220.0}},
-        {0, {0.0, 0.0, 230.0}}
+        {0, {0.0, 0.0, -180.0}},
+        {0, {0.0, 0.0, -190.0}},
+        {0, {0.0, 0.0, -200.0}},
+        {0, {0.0, 0.0, -210.0}},
+        {0, {0.0, 0.0, -220.0}},
+        {0, {0.0, 0.0, -230.0}}
     });
     std::vector<std::string> parameters{"effectiveConfiningStress"};
     auto stress = genericModel(argv[1], query, parameters);
 
-    double curVal = 200.0;
+    double curVal = -180;
     for (auto& s : stress[0]) {
-        constexpr double eps = 10e-12;
+        constexpr double eps = 10e-15;
         assert(std::abs(s - effectiveConfiningStress(curVal)) <= eps);
-        curVal += 10.0;
+        curVal -= 10.0;
     }
 
 
