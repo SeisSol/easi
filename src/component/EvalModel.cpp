3c00366320eecb459da2cb39759782313a1fd5f9
David Schneller <david.schneller@tum.de>
2025-01-24 20:07:57 +0100

    Allocate new indices for EvalModel

--- src/component/EvalModel.cpp
+++ src/component/EvalModel.cpp
@@ -10,6 +10,13 @@ namespace easi {
 void EvalModel::evaluate(Query& query, ResultAdapter& result) {
     Query subQuery = query.shallowCopy();
 
+    // create a new index vector here (do not override the old one for the Composite::evaluate later)
+    Vector<unsigned> newIndices(subQuery.x.rows());
+    for (unsigned i = 0; i < subQuery.x.rows(); ++i) {
+        newIndices(i) = i;
+    }
+    subQuery.index = newIndices;
+
     Matrix<double> y(subQuery.x.rows(), dimCodomain());
     ArraysAdapter<> adapter;
     unsigned col = 0;
